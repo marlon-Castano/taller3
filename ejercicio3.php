d334a7e67292942e1a413177ee4c6be8dd0b4b30
Davidelgrande <dcammrt34@gmail.com>
2020-10-21 16:22:04 -0500

    marlon es re gai

--- ejercicio3.php
+++ ejercicio3.php
@@ -1,4 +1,16 @@
-<?php
+<!DOCTYPE html lang="en" xmlns="http://www.w3.org/1999/xhtml">
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TALLER 3 APP</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container pt-5"><h2 align="center"> Tabla de los números pares del 1 al 100<h2> </div>
+    
+
+    <?php
  # Hacer un programa que imprima los números pares entre el 0 y 100
 
  $num= 100; 
@@ -19,3 +31,11 @@
 
 
 ?> 
+</body>
+</html>
+
+
+
+
+
+
